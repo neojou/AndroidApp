f7f865088443b8d79a75c69b329102b3de5aafd8
neojou <neojou@gmail.com>
2022-02-20 23:38:53 +0800

    Head First Kotlin : chap1
    MyFirstApp
    
    Signed-off-by: neojou <neojou@gmail.com>

---
+++ Kotlin/HeadFirstKotlin/chap1/MyFirstApp/src/App.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var x = 1
+
+    // REPL Read-Eval-Print Loop
+    while (x < 3) {
+        print("Co")
+        print(if (x == 1) "Ca" else "La")
+        print(" ")
+        x = x + 1
+    }
+    println()
+}
