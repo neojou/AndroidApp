40109cf722f60cc5d317637988300b37873ae2c5
neojou <neojou@gmail.com>
2022-11-19 23:52:05 +0800

    JLPTtrainer: change to use Fragment
    Prepare to import questions from txt file to database.
    - Buttons in main activity
    - question in one Fragment
    - will create another Frament for setting, and make a button for import.
    
    Signed-off-by: neojou <neojou@gmail.com>

---
+++ JLPTtrainer/app/src/main/java/com/nj/jlpttrainer/FragmentQuestion.java
@@ -0,0 +1,196 @@
+package com.nj.jlpttrainer;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import com.nj.jlpttrainer.databinding.FragmentQuestionBinding;
+
+
+
+import java.util.List;
+import java.lang.Integer;
+
+
+public class FragmentQuestion extends Fragment
+{
+    private static final String TAG="JLPT_trainer:FragmentQuestion";
+    private QuestionDataViewModel q_dvm;
+
+    FragmentQuestionBinding binding;
+
+    Question q = new Question();
+    TextView question_title;
+    TextView question_content;
+    TextView choice_title;
+    RadioGroup choice_rd;
+    RadioButton[] choice_rb = new RadioButton[4];
+    String[] choices = new String[4];
+    int q_id;
+
+    public FragmentQuestion(QuestionDataViewModel m)
+    {
+        super(R.layout.fragment_question);
+        q_dvm = m;
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+        ) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_question, container, false);
+        setViewItemsBinding();
+        binding.setQDvm(q_dvm);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        /* setVisibility : View.VISIBLE, View.INVISIBLE, View.GONE */
+        //question_title.setVisibility(View.GONE);
+        //choice_title.setVisibility(View.GONE);
+        //choice_rd.setVisibility(View.GONE);
+
+        /* once q_dvm finished commands, isLoading is set to false,
+         *  and can start to click the buttons.
+         */
+        /*
+        q_dvm.issueGetAllQuestions(new QuestionDataViewModel.onDataReadyCallback() {
+            @Override
+            public void onDataReady(List<Question> lq) {
+                Log.v(TAG, "onDataReady");
+                q_dvm.questions = lq;
+                q_dvm.total_questions = q_dvm.questions.size();
+                q_dvm.isLoading.set(false);
+            }
+        });
+*/
+        set_screen_without_question();
+    }
+
+    /*
+    private void setViewItems() {
+        question_title = (TextView)findViewById(R.id.question_title);
+        question_content = (TextView)findViewById(R.id.question_content);
+        choice_title = (TextView)findViewById(R.id.choice_title);
+        choice_rd = (RadioGroup)findViewById(R.id.choices);
+        choice_rb[0] = (RadioButton)findViewById(R.id.choice1);
+        choice_rb[1] = (RadioButton)findViewById(R.id.choice2);
+        choice_rb[2] = (RadioButton)findViewById(R.id.choice3);
+        choice_rb[3] = (RadioButton)findViewById(R.id.choice4);
+        button_answer = (Button)findViewById(R.id.button_answer);
+        button_next = (Button)findViewById(R.id.button_next);
+        button_setting = (Button)findViewById(R.id.button_setting);
+    }
+
+     */
+
+    private void setViewItemsBinding() {
+        question_title = binding.questionTitle;
+        question_content = binding.questionContent;
+        choice_title = binding.choiceTitle;
+        choice_rd = binding.choices;
+        choice_rb[0] = binding.choice1;
+        choice_rb[1] = binding.choice2;
+        choice_rb[2] = binding.choice3;
+        choice_rb[3] = binding.choice4;
+    }
+
+    private void set_question_to_view(Question q) {
+        question_title.setText(getString(R.string.each_question_title, q.id));
+        question_content.setText(q.question);
+        choice_title.setText(getString(R.string.choice_title));
+        choice_rd.clearCheck();
+        choices[0] = q.choice1;
+        choices[1] = q.choice2;
+        choices[2] = q.choice3;
+        choices[3] = q.choice4;
+        choice_rb[0].setText(choices[0]);
+        choice_rb[1].setText(choices[1]);
+        choice_rb[2].setText(choices[2]);
+        choice_rb[3].setText(choices[3]);
+        set_radio_button_color(choice_rb[0], R.color.gray);
+        set_radio_button_color(choice_rb[1], R.color.gray);
+        set_radio_button_color(choice_rb[2], R.color.gray);
+        set_radio_button_color(choice_rb[3], R.color.gray);
+        set_screen_with_question();
+    }
+
+
+
+    private void set_radio_button_color(RadioButton rb, int color) {
+        rb.setTextColor(ContextCompat.getColorStateList(getContext(), color));
+    }
+
+    private void set_right_choice_button(int which) {
+        int id = which - 1;
+        if (id < 0 || id > 3) return;
+        set_radio_button_color(choice_rb[id], R.color.blue);
+        //choice[id].setChecked(true);
+    }
+
+    private void set_wrong_choice_button(int which) {
+        int id = which - 1;
+        if (id < 0 || id > 3) return;
+        set_radio_button_color(choice_rb[id], R.color.fuchsia);
+    }
+
+    private void set_screen_without_question() {
+        question_content.setVisibility(View.INVISIBLE);
+        choice_title.setVisibility(View.INVISIBLE);
+        choice_rd.setVisibility(View.INVISIBLE);
+    }
+
+    private void set_screen_with_question() {
+        question_content.setVisibility(View.VISIBLE);
+        choice_title.setVisibility(View.VISIBLE);
+        choice_rd.setVisibility(View.VISIBLE);
+    }
+
+
+    /*
+    private void init_db() {
+        Question question = new Question();
+        question.set_to_q1();
+        q_rep.insert(question);
+        question = new Question();
+        question.set_to_q2();
+        q_rep.insert(question);
+        question = new Question();
+        question.set_to_q3();
+        q_rep.insert(question);
+        question = new Question();
+        question.set_to_q4();
+        q_rep.insert(question);
+        question = new Question();
+        question.set_to_q5();
+        q_rep.insert(question);
+        question = new Question();
+        question.set_to_q6();
+        q_rep.insert(question);
+    }
+
+     */
+}
+
--- JLPTtrainer/app/src/main/java/com/nj/jlpttrainer/TestActivity.java
+++ JLPTtrainer/app/src/main/java/com/nj/jlpttrainer/TestActivity.java
@@ -1,132 +1,72 @@
 package com.nj.jlpttrainer;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.databinding.DataBindingUtil;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.RadioGroup;
-import android.widget.RadioButton;
 
-import java.lang.Integer;
-import java.util.ArrayList;
-import java.util.List;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 
 import com.nj.jlpttrainer.databinding.ActivityMainBinding;
 
+import java.util.List;
+
 
 public class TestActivity extends AppCompatActivity
-    implements View.OnClickListener
+        implements View.OnClickListener
 {
     private static final String TAG="JLPT_trainer:TestActivity";
+
+    FragmentQuestion frag_question;
     private ActivityMainBinding binding;
     private QuestionDataViewModel q_dvm;
 
-    Question q = new Question();
-    TextView question_title;
-    TextView question_content;
-    TextView choice_title;
-    RadioGroup choice_rd;
-    RadioButton[] choice_rb = new RadioButton[4];
+
     Button button_answer;
     Button button_next;
     Button button_setting;
-    String[] choices = new String[4];
+
     boolean isStarted;
     boolean isFinished;
-    int q_id;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        //setViewItems();
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        setViewItemsBinding();
 
-        q_dvm = new QuestionDataViewModel(getApplication());
-        binding.setQDvm(q_dvm);
+        if (savedInstanceState == null) {
+            q_dvm = new QuestionDataViewModel(getApplication());
 
-        isStarted = false;
-        isFinished = false;
-        q_id = 0;
-        button_next.setText(getString(R.string.button_start));
-        set_button_click_listener();
+            binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+            setViewItemsBinding();
+            binding.setQDvm(q_dvm);
+            frag_question = new FragmentQuestion(q_dvm);
 
-        /* setVisibility : View.VISIBLE, View.INVISIBLE, View.GONE */
-        //question_title.setVisibility(View.GONE);
-        //choice_title.setVisibility(View.GONE);
-        //choice_rd.setVisibility(View.GONE);
+            isStarted = false;
+            isFinished = false;
+            frag_question.q_id = 0;
+            button_next.setText(getString(R.string.button_start));
+            set_button_click_listener();
 
-        /* once q_dvm finished commands, isLoading is set to false,
-         *  and can start to click the buttons.
-         */
-        q_dvm.issueGetAllQuestions(new QuestionDataViewModel.onDataReadyCallback() {
-            @Override
-            public void onDataReady(List<Question> lq) {
-                Log.v(TAG, "onDataReady");
-                q_dvm.questions = lq;
-                q_dvm.total_questions = q_dvm.questions.size();
-                q_dvm.isLoading.set(false);
-            }
-        });
+/*
+            FragmentManager fManager = getSupportFragmentManager();
+            FragmentTransaction ft = fManager.beginTransaction();
+            ft.add(R.id.fragment_main, frag_question);
+            ft.replace(R.id.fragment_main, frag_question);
+            ft.commit();
 
-        set_screen_without_question();
+ */
+        }
     }
-
-    /*
-    private void setViewItems() {
-        question_title = (TextView)findViewById(R.id.question_title);
-        question_content = (TextView)findViewById(R.id.question_content);
-        choice_title = (TextView)findViewById(R.id.choice_title);
-        choice_rd = (RadioGroup)findViewById(R.id.choices);
-        choice_rb[0] = (RadioButton)findViewById(R.id.choice1);
-        choice_rb[1] = (RadioButton)findViewById(R.id.choice2);
-        choice_rb[2] = (RadioButton)findViewById(R.id.choice3);
-        choice_rb[3] = (RadioButton)findViewById(R.id.choice4);
-        button_answer = (Button)findViewById(R.id.button_answer);
-        button_next = (Button)findViewById(R.id.button_next);
-        button_setting = (Button)findViewById(R.id.button_setting);
-    }
-
-     */
 
     private void setViewItemsBinding() {
-        question_title = binding.questionTitle;
-        question_content = binding.questionContent;
-        choice_title = binding.choiceTitle;
-        choice_rd = binding.choices;
-        choice_rb[0] = binding.choice1;
-        choice_rb[1] = binding.choice2;
-        choice_rb[2] = binding.choice3;
-        choice_rb[3] = binding.choice4;
         button_answer = binding.buttonAnswer;
         button_next = binding.buttonNext;
         button_setting = binding.buttonSetting;
-    }
-
-    private void set_question_to_view(Question q) {
-        question_title.setText(getString(R.string.each_question_title, q.id));
-        question_content.setText(q.question);
-        choice_title.setText(getString(R.string.choice_title));
-        choice_rd.clearCheck();
-        choices[0] = q.choice1;
-        choices[1] = q.choice2;
-        choices[2] = q.choice3;
-        choices[3] = q.choice4;
-        choice_rb[0].setText(choices[0]);
-        choice_rb[1].setText(choices[1]);
-        choice_rb[2].setText(choices[2]);
-        choice_rb[3].setText(choices[3]);
-        set_radio_button_color(choice_rb[0], R.color.gray);
-        set_radio_button_color(choice_rb[1], R.color.gray);
-        set_radio_button_color(choice_rb[2], R.color.gray);
-        set_radio_button_color(choice_rb[3], R.color.gray);
-        set_screen_with_question();
     }
 
     private void set_button_click_listener() {
@@ -143,43 +83,13 @@ public class TestActivity extends AppCompatActivity
         else if (id == R.id.button_next)
             next_question();
         else if (id == R.id.button_setting)
-            choice_title.setText("Setting");
-    }
-
-    private void set_radio_button_color(RadioButton rb, int color) {
-        rb.setTextColor(ContextCompat.getColorStateList(this, color));
-    }
-
-    private void set_right_choice_button(int which) {
-        int id = which - 1;
-        if (id < 0 || id > 3) return;
-        set_radio_button_color(choice_rb[id], R.color.blue);
-        //choice[id].setChecked(true);
-    }
-
-    private void set_wrong_choice_button(int which) {
-        int id = which - 1;
-        if (id < 0 || id > 3) return;
-        set_radio_button_color(choice_rb[id], R.color.fuchsia);
-    }
-
-    private void set_screen_without_question() {
-        question_content.setVisibility(View.INVISIBLE);
-        choice_title.setVisibility(View.INVISIBLE);
-        choice_rd.setVisibility(View.INVISIBLE);
-        button_answer.setEnabled(false);
-        button_next.setEnabled(true);
-    }
-
-    private void set_screen_with_question() {
-        question_content.setVisibility(View.VISIBLE);
-        choice_title.setVisibility(View.VISIBLE);
-        choice_rd.setVisibility(View.VISIBLE);
-        button_answer.setEnabled(true);
-        button_next.setEnabled(false);
+            setting();
+            //choice_title.setText("Setting");
     }
 
     private void check_answer() {
+        Log.v(TAG, "check_answer");
+        /*
         if (isStarted == false)
             return;
         if (isFinished == true)
@@ -206,41 +116,21 @@ public class TestActivity extends AppCompatActivity
         if (answer != q.right_choice) {
             set_wrong_choice_button(answer);
             choice_title.setText(getString(R.string.choice_title) + " : " +
-                                getString(R.string.wrong));
+                    getString(R.string.wrong));
         } else {
             choice_title.setText(getString(R.string.choice_title) + " : " +
-                                getString(R.string.correct));
+                    getString(R.string.correct));
         }
         button_answer.setEnabled(false);
         button_next.setEnabled(true);
+
+         */
     }
 
-    /*
-    private void init_db() {
-        Question question = new Question();
-        question.set_to_q1();
-        q_rep.insert(question);
-        question = new Question();
-        question.set_to_q2();
-        q_rep.insert(question);
-        question = new Question();
-        question.set_to_q3();
-        q_rep.insert(question);
-        question = new Question();
-        question.set_to_q4();
-        q_rep.insert(question);
-        question = new Question();
-        question.set_to_q5();
-        q_rep.insert(question);
-        question = new Question();
-        question.set_to_q6();
-        q_rep.insert(question);
-    }
-
-     */
 
     private void next_question() {
-        //Log.v(TAG, "next_question");
+        Log.v(TAG, "next_question");
+        /*
         int total_questions = q_dvm.total_questions;
 
         if (isFinished)
@@ -270,9 +160,14 @@ public class TestActivity extends AppCompatActivity
         if (isFinished == false) {
             q = generate_question();
         }
+
+         */
     }
 
-    private Question generate_question() {
+    private void generate_question() {
+        Log.v(TAG, "generate_question");
+
+        /*
         List<Question> questions = q_dvm.questions;
 
         if (questions == null || questions.isEmpty() ) {
@@ -295,6 +190,12 @@ public class TestActivity extends AppCompatActivity
 
         set_question_to_view(q);
         return q;
-    }
-}
 
+         */
+    }
+
+    private void setting() {
+        Log.v(TAG, "setting");
+    }
+
+}
