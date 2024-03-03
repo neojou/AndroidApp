3d9b29ca9cf4fb337fa63bb5492b7e65873e3c54
neojou <neojou@gmail.com>
2024-03-03 11:54:47 +0800

    JPFruits: Use Fragment
    Signed-off-by: neojou <neojou@gmail.com>

--- JPFruits/app/src/main/java/com/nj/jpfruits/JPFruitActivity.java
+++ JPFruits/app/src/main/java/com/nj/jpfruits/JPFruitActivity.java
@@ -9,16 +9,40 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 
+
 public class JPFruitActivity extends AppCompatActivity {
     private static final String TAG="JPFruits::JPFruitActivity";
+    FragmentManager fragment_manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            setContentView(R.layout.activity_main);
+            ft_start();
+        }
+    }
+
+    private void ft_start() {
+        fragment_manager = getSupportFragmentManager();
+        if (fragment_manager == null) {
+            Log.e(TAG, "fragment_manager is null");
+            return;
+        }
+
+        FragmentImage frag_image = new FragmentImage();
+
+        FragmentTransaction ft;
+        ft = fragment_manager.beginTransaction();
+        ft.replace(R.id.fragment_activity_main, frag_image);
+        ft.commit();
     }
 }
+
